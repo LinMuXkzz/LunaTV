80ed53dc511a1f5fe2924c011dbd07f66d2764c3
LinMuXkzz <2980524834@qq.com>
2026-04-10 23:27:03 +0800

    Add files via upload

--- src/app/api/admin/online-users/route.ts
+++ src/app/api/admin/online-users/route.ts
@@ -2,6 +2,8 @@
 
 import { NextRequest, NextResponse } from 'next/server';
 
+export const dynamic = 'force-dynamic';
+
 import { getAuthInfoFromCookie } from '@/lib/auth';
 import { db } from '@/lib/db';
 
